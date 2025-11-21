2952cb1234123f3e41e4b70a2dafc9009321cfa1
rahuljaicsam <79331747+rahuljaicsam@users.noreply.github.com>
2025-11-21 11:19:20 +0000

    edit

--- lib/models/provider_type.dart
+++ lib/models/provider_type.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
 
 enum ProviderType {
-  doctor(Color(0xFF42A5F5)),
-  nurse(Color(0xFF66BB6A)),
-  physiotherapist(Color(0xFFFFA726)),
-  caregiver(Color(0xFFEF5350)),
-  counselor(Color(0xFF26C6DA)),
-  other(Color(0xFF9E9E9E));
+  doctor,
+  nurse,
+  physiotherapist,
+  caregiver,
+  counselor,
+  other;
 
-  final Color color;
-  const ProviderType(this.color);
+  // Add display name
+  String get displayName {
+    switch (this) {
+      case ProviderType.doctor:
+        return 'Doctor';
+      case ProviderType.nurse:
+        return 'Nurse';
+      case ProviderType.physiotherapist:
+        return 'Physiotherapist';
+      case ProviderType.caregiver:
+        return 'Caregiver';
+      case ProviderType.counselor:
+        return 'Counselor';
+      case ProviderType.other:
+        return 'Other';
+    }
+  }
+
+  // Add color (if needed)
+  Color get color {
+    switch (this) {
+      case ProviderType.doctor:
+        return Color(0xFF42A5F5);
+      case ProviderType.nurse:
+        return Color(0xFF66BB6A);
+      case ProviderType.physiotherapist:
+        return Color(0xFFFFA726);
+      case ProviderType.caregiver:
+        return Color(0xFFEF5350);
+      case ProviderType.counselor:
+        return Color(0xFF26C6DA);
+      case ProviderType.other:
+        return Color(0xFF9E9E9E);
+    }
+  }
 }
